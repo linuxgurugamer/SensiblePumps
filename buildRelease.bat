19ed0a4a3d366a09501fbef73678f295726a87b6
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-11-07 18:17:06 -0500

    Compiled for 1.2, build scripts crated

---
+++ buildRelease.bat
@@ -0,0 +1,50 @@
+﻿@echo off
+set DEFHOMEDRIVE=d:
+set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
+set HOMEDIR=
+set HOMEDRIVE=%CD:~0,2%
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+echo Default homedir: %DEFHOMEDIR%
+
+rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
+
+if "%HOMEDIR%" == "" (
+set HOMEDIR=%DEFHOMEDIR%
+)
+echo %HOMEDIR%
+
+SET _test=%HOMEDIR:~1,1%
+if "%_test%" == ":" (
+set HOMEDRIVE=%HOMEDIR:~0,2%
+)
+
+type SensiblePumpsCont.version
+set /p VERSION= "Enter version: "
+
+set d=%HOMEDIR\install
+if exist %d% goto one
+mkdir %d%
+:one
+set d=%HOMEDIR%\install\Gamedata
+if exist %d% goto two
+mkdir %d%
+:two
+
+rmdir /s /q %HOMEDIR%\install\Gamedata\SensiblePumpsCont
+
+copy SensiblePumps\bin\Release\SensiblePumpsCont.dll GameData\SensiblePumpsCont\Plugins
+copy  SensiblePumpsCont.version GameData\SensiblePumpsCont\SensiblePumpsCont.version
+copy /Y README.md GameData\SensiblePumpsCont
+
+xcopy /Y /E GameData\SensiblePumpsCont  %HOMEDIR%\install\Gamedata\SensiblePumpsCont\
+copy /y LICENSE.md %HOMEDIR%\install\Gamedata\SensiblePumpsCont
+copy /y ..\MiniAVC.dll %HOMEDIR%\install\GameData\SensiblePumpsCont
+
+%HOMEDRIVE%
+cd %HOMEDIR%\install
+
+set FILE="%RELEASEDIR%\SensiblePumpsCont-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% GameData\SensiblePumpsCont 
---
+++ deploy.bat
@@ -0,0 +1,18 @@
+﻿
+rem copy /Y "$(TargetDir)$(TargetFileName)" "D:\Users\jbb\github\KerbalObjectInspector\KerbalObjectInspector\Plugins\$(TargetFileName)"
+
+rem copy /Y "D:\Users\jbb\github\KerbalObjectInspector\Source\KerbalObjectInspector\README.txt" "D:\Users\jbb\github\KerbalObjectInspector\KerbalObjectInspector\README.txt"
+
+rem copy /Y "D:\Users\jbb\github\KerbalObjectInspector\KerbalObjectInspector\README.txt" "D:\Users\jbb\github\KerbalObjectInspector\README.md"
+
+rem copy /Y "$(TargetDir)$(TargetFileName)" "D:\Users\jbb\github\KerbalObjectInspector\KerbalObjectInspector\Plugins\$(TargetFileName)"
+
+
+set H=R:\KSP_1.1.4_dev
+echo %H%
+
+copy SensiblePumps\bin\Debug\SensiblePumps.dll D:\Users\jbb\github\SensiblePumps\GameData\SensiblePumpsCont\Plugins
+copy install-sensible-pumps.cfg D:\Users\jbb\github\SensiblePumps\GameData\SensiblePumpsCont
+
+mkdir %H%\GameData\SensiblePumpsCont
+xcopy GameData\SensiblePumpsCont %H%\GameData\SensiblePumpsCont\  /E /Y
