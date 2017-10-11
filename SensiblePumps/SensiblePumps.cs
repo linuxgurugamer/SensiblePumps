a4611280e9ef6d5c5a9ee9d6cf308b126fcae7b8
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-10-11 12:26:16 -0400

    Fixed MM script to add this to engines with ModuleEngines FX Added code to exclude solid rocket motors

--- SensiblePumps/SensiblePumps.cs
+++ SensiblePumps/SensiblePumps.cs
@@ -1,48 +1,73 @@
 ﻿
 namespace SensiblePumps
 {
-	public class SensiblePumps: PartModule
-	{
+    public class SensiblePumps : PartModule
+    {
 
-		[UI_Toggle (disabledText = "off", enabledText = "on")]
-		[KSPField (guiActiveEditor = true, guiName = "Sensible Pumps ", isPersistant = true)]
-		public bool isSensible = true;
+        [UI_Toggle(disabledText = "off", enabledText = "on")]
+        [KSPField(guiActiveEditor = true, guiName = "Sensible Pumps ", isPersistant = true)]
+        public bool isSensible = true;
 
-		private int numberOfParts;
+        private int numberOfParts;
 
-		public override void OnStart (StartState state)
-		{
-			if (state != StartState.Editor) {
-				numberOfParts = vessel.parts.Count;
-			}
-		}
+        bool isSRB = false;
 
-		// Ok, I can't figure out if there is a callback I can use or not, screw it, it will work anyway.
-		public override void OnFixedUpdate ()
-		{
-			if (isSensible) {
-				if (numberOfParts != vessel.parts.Count) {
-					numberOfParts = vessel.parts.Count;
-					// Number of parts changed, so, let's go to work.
-					// If we're part of a debris object, shutdown all engines.
-					if (!vessel.isCommandable) {
-						foreach (PartModule thatModule in part.Modules) {
-							var thatEngine = thatModule as ModuleEngines;
-							var thatEngineFX = thatModule as ModuleEnginesFX;
-							if (thatEngine != null) {
-								thatEngine.Shutdown ();
-							}
-							if (thatEngineFX != null) {
-								thatEngineFX.Shutdown ();
-							}
-						}
-					}
-					// And selfdestruct the partmodule for good measure, we have no further work to do.
-					Destroy (this);
-				}
-			}
-		}
+        public override void OnStart(StartState state)
+        {
+            if (state != StartState.Editor)
+            {
+                numberOfParts = vessel.parts.Count;
+            }
+            foreach (PartModule thatModule in part.Modules)
+            {
+                var thatEngine = thatModule as ModuleEngines;
+                var thatEngineFX = thatModule as ModuleEnginesFX;
+                if (thatEngine != null)
+                {
+                    for (int i = 0; i < thatEngine.propellants.Count; i++)
+                        if (thatEngine.propellants[i].name == "SolidFuel")
+                            isSRB = true;
+                }
+                if (thatEngineFX != null)
+                {
+                    for (int i = 0; i < thatEngineFX.propellants.Count; i++)
+                        if (thatEngineFX.propellants[i].name == "SolidFuel")
+                            isSRB = true;
+                }
+            }
+        }
 
-	}
+        // Ok, I can't figure out if there is a callback I can use or not, screw it, it will work anyway.
+        public override void OnFixedUpdate()
+        {
+            if (isSensible)
+            {
+                if (numberOfParts != vessel.parts.Count)
+                {
+                    numberOfParts = vessel.parts.Count;
+                    // Number of parts changed, so, let's go to work.
+                    // If we're part of a debris object, shutdown all engines.
+                    if (!vessel.isCommandable && !isSRB)
+                    {
+                        foreach (PartModule thatModule in part.Modules)
+                        {
+                            var thatEngine = thatModule as ModuleEngines;
+                            var thatEngineFX = thatModule as ModuleEnginesFX;
+
+                            if (thatEngine != null)
+                                thatEngine.Shutdown();
+                       
+                            if (thatEngineFX != null)
+                                thatEngineFX.Shutdown();
+
+                        }
+                    }
+                    // And selfdestruct the partmodule for good measure, we have no further work to do.
+                    Destroy(this);
+                }
+            }
+        }
+
+    }
 }
 
